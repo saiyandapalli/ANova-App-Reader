b4dda1102bb297e8682fbdfb6d6b68517637c493
Sai Yandapalli <saiyandapalli07@berkeley.edu>
2020-04-16 23:25:34 -0700

    added global

---
+++ appreader/src/global.js
@@ -0,0 +1,6 @@
+// For global variables.
+// The global scope in React Native is variable global.
+// For ex: as global.foo = foo, then you can use global.foo anywhere as a global variable.
+// Make sure you import './global.js' in your files to use global variables!
+
+global.example = 123;
