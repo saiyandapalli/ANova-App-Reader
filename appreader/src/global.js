80956b8f144230da677cf97b3be2f59b9f29e54f
unknown <saiyandapalli07@gmail.com>
2020-04-17 19:46:29 -0700

    ADDING GLOBALS

--- appreader/src/global.js
+++ appreader/src/global.js
@@ -3,4 +3,7 @@
 // For ex: as global.foo = foo, then you can use global.foo anywhere as a global variable.
 // Make sure you import './global.js' in your files to use global variables!
 
-global.example = 123;
+global.APPLICATION_KEY = "link"; // Applications airtable link/key
+global.DECISION_KEY = "link"; // Applications airtable link/key
+global.OFFICERS = ["sai", "maggie", "joy"];
+global.NUM_YES = 30;
