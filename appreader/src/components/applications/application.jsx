6fe475de4cf026db8dc95bfb9f8a437d8ebab1a0
Aditya Varshney <adityavarshney@gmail.com>
2020-04-22 21:35:07 -0700

    airtableVoteHandler working

--- appreader/src/components/applications/application.jsx
+++ appreader/src/components/applications/application.jsx
@@ -11,6 +11,7 @@ class Applications extends Component {
       isLoaded: false,
       userDecisions: [],
       allApplications: [],
+      remainingApps: [],
       numYeses: null,
       reviewerName: "test", // TODO: keep track of the current user
     }
@@ -28,11 +29,63 @@ class Applications extends Component {
   }
 
   getNumYeses() {
-    return global.NUM_YES - this.state.userDecisions.filter(r => r.fields['Interview'] === "Yes");
+    return this.state.numYeses;
+  }
+
+  airtableVoteHandler(applicantName, reviewerName, vote, flag, comments, id) {
+    fetch(global.DECISIONS_URL, {
+      body: "{\"records\": [{\"fields\": {\"Applicant Name\": \""+applicantName+"\",\"Reviewer Name\": \""+reviewerName+"\",\"Interview\": \""+vote+"\",\"Flag\": \""+flag+"\",\"Comments\": \""+comments+"\", \"ID\": \""+id+"\"}}]}",
+      headers: {
+        Authorization: "Bearer " + global.AIRTABLE_KEY,
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    });
+
+    fetch(global.DECISIONS_URL + "?filterByFormula=%7BReviewer%20Name%7D%20%3D%20%20%22"+reviewerName+"%22&view=Grid%20view", {
+      headers: {
+        Authorization: "Bearer " + global.AIRTABLE_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            userDecisions: result.records,
+          });
+        }, 
+        (error) => {
+          this.setState({
+            error,
+          });
+        }
+      );
+    
+    fetch(global.APPLICATIONS_URL + "?view=Grid%20view", {
+      headers: {
+        Authorization: "Bearer " + global.AIRTABLE_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState((state) => {return {
+            isLoaded: true,
+            allApplications: this.shuffle(result.records),
+            numYeses: global.NUM_YES - state.userDecisions.filter(r => r.fields['Interview'] === "Yes").length,
+          }});
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
 
   componentDidMount() {
-
+    
     const reviewerName = this.state.reviewerName;
 
     fetch(global.DECISIONS_URL + "?filterByFormula=%7BReviewer%20Name%7D%20%3D%20%20%22"+{reviewerName}+"%22&view=Grid%20view", {
@@ -74,6 +127,10 @@ class Applications extends Component {
           });
         }
       );  
+
+      this.setState({
+        numYeses: global.NUM_YES - this.state.userDecisions.filter(r => r.fields['Interview'] === "Yes")
+      })
   }
 
   formatFieldResponse(entry) {
@@ -116,6 +173,8 @@ class Applications extends Component {
     const appsLeft = remainingApps.length;
     const numYeses = this.getNumYeses();
 
+    console.log(numYeses)
+
     // TODO: read these from the actual fields
     const flag = "No";
     const comments = "";
@@ -143,28 +202,12 @@ class Applications extends Component {
         </div>
 
         <div className="vote">
-          <button className="no-button" onClick={() => {
-            fetch(global.DECISIONS_URL, {
-              body: "{\"records\": [{\"fields\": {\"Applicant Name\": \""+applicantName+"\",\"Reviewer Name\": \""+reviewerName+"\",\"Interview\": \"No\",\"Flag\": \""+flag+"\",\"Comments\": \""+comments+"\", \"ID\": \""+id+"\"}}]}",
-              headers: {
-                Authorization: "Bearer " + global.AIRTABLE_KEY,
-                "Content-Type": "application/json"
-              },
-              method: "POST"
-            });
-            window.location.reload();}}>No</button>
+          <button className="no-button" onClick={() => 
+            this.airtableVoteHandler(applicantName, reviewerName, "No", flag, comments, id)}>No</button>
           <button className="no-button" onClick={() => 
             window.location.reload()}>Skip</button>
-          <button className="no-button" onClick={() => {
-            fetch("https://api.airtable.com/v0/appm1EwjHL56mOmPx/Decisions", {
-              body: "{\"records\": [{\"fields\": {\"Applicant Name\": \""+applicantName+"\",\"Reviewer Name\": \""+reviewerName+"\",\"Interview\": \"Yes\",\"Flag\": \""+flag+"\",\"Comments\": \""+comments+"\", \"ID\": \""+id+"\"}}]}",
-              headers: {
-                Authorization: "Bearer keyd6qkfF0Q6csT9G",
-                "Content-Type": "application/json"
-              },
-              method: "POST"
-            });
-          window.location.reload();}}>Yes</button>
+          <button className="no-button" onClick={() => 
+            this.airtableVoteHandler(applicantName, reviewerName, "Yes", flag, comments, id)}>Yes</button>
         </div>
       </div>
     );
