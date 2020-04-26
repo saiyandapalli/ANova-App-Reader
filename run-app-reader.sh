ebcb4efe639afbd7f401d15eccb1a150a4705ee8
Aditya Varshney <adityavarshney@gmail.com>
2020-04-25 21:44:38 -0700

    whoops now adding run-app-reader.sh

---
+++ run-app-reader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "(sh) installing node js";
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/";
+cd appreader;
+echo "(sh) starting app reader!"
+npm install; 
+npm start;
